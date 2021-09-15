549f370065336d76fa40d0ce5d4f5f22e962c038
AnkitKrRoy <72970183+AnkitKrRoy@users.noreply.github.com>
2021-09-15 19:22:49 +0530

    Add files via upload

---
+++ python drink_water/drink_water_notification.py
@@ -0,0 +1,10 @@
+import time
+from plyer import notification
+
+if __name__ == "__main__":
+    notification.notify(
+        title = "please drinnk water ",
+        message = "Drinking water, also known as potable water, is water that is safe to drink or use for food preparation. The amount of drinking water required to maintain good health varies, and depends on physical activity level, age, health-related issues",
+        app_icon = "E:\python projects/python drink_water/icon.ico",
+        timeout = 10
+)
